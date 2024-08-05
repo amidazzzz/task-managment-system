15c7505a2f0c11a0447038fb5230324d759a88a8
amida <cain.wells@mail.ru>
2024-08-05 18:32:02 +0300

    Added register and login post methods in user controller

--- src/main/java/org/amida/task_managment_system/user_managment/controller/UserController.java
+++ src/main/java/org/amida/task_managment_system/user_managment/controller/UserController.java
@@ -1,2 +1,50 @@
-package org.amida.task_managment_system.user_managment.controller;public class UserController {
+package org.amida.task_managment_system.user_managment.controller;
+
+import org.amida.task_managment_system.user_managment.model.User;
+import org.amida.task_managment_system.user_managment.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/welcome")
+    public String welcome(){
+        return "Hello";
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return ResponseEntity.ok("User authenticated successfully");
+    }
+
+
 }
