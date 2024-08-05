a5ef356df15d1feaf043e22a3588b55c4e904c0e
amida <cain.wells@mail.ru>
2024-08-05 18:32:22 +0300

    Managed user repository

--- src/main/java/org/amida/task_managment_system/user_managment/repository/UserRepository.java
+++ src/main/java/org/amida/task_managment_system/user_managment/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package org.amida.task_managment_system.user_managment.repository;public class UserRepository {
+package org.amida.task_managment_system.user_managment.repository;
+
+import org.amida.task_managment_system.user_managment.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
