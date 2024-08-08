8b7838385f6e3e28dff6aa979ce9b7c451fc48d0
amida <cain.wells@mail.ru>
2024-08-08 23:40:01 +0300

    Added resource not found exception

---
+++ src/main/java/org/amida/task_managment_system/swagger/config/OpenApiConfig.java
@@ -0,0 +1,2 @@
+package org.amida.task_managment_system.swagger.config;public class OpenApiConfig {
+}
--- src/main/java/org/amida/task_managment_system/task_managment/exception/ResourceNotFoundException.java
+++ src/main/java/org/amida/task_managment_system/task_managment/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package org.amida.task_managment_system.exception;
+package org.amida.task_managment_system.task_managment.exception;
 
 public class ResourceNotFoundException extends Exception{
     public ResourceNotFoundException(String errorMessage){
