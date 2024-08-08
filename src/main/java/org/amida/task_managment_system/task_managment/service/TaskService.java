176a2addd13701a7e76993b3f9238108fd8d1fff
amida <cain.wells@mail.ru>
2024-08-08 19:53:24 +0300

    Managed task service

--- src/main/java/org/amida/task_managment_system/task_managment/service/TaskService.java
+++ src/main/java/org/amida/task_managment_system/task_managment/service/TaskService.java
@@ -1,11 +1,12 @@
-package org.amida.task_managment_system.service;
+package org.amida.task_managment_system.task_managment.service;
 
 import lombok.RequiredArgsConstructor;
-import org.amida.task_managment_system.exception.ResourceNotFoundException;
-import org.amida.task_managment_system.repository.CommentRepository;
-import org.amida.task_managment_system.repository.TaskRepository;
+import org.amida.task_managment_system.task_managment.exception.ResourceNotFoundException;
+import org.amida.task_managment_system.task_managment.repository.CommentRepository;
+import org.amida.task_managment_system.task_managment.repository.TaskRepository;
 import org.amida.task_managment_system.task_managment.model.Comment;
 import org.amida.task_managment_system.task_managment.model.Task;
+import org.amida.task_managment_system.user_managment.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,16 +22,33 @@ public class TaskService {
         return taskRepository.findById(id).orElse(null);
     }
 
+    public List<Task> getTaskByAuthorId(Long authorId) {
+        return taskRepository.findByAuthorId(authorId);
+    }
+
+    public List<Task> findByAssigneeId(Long assigneeId) {
+        return taskRepository.findByAssigneeId(assigneeId);
+    }
+
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
 
-    public Task saveTask(Task task) {
+    public Task createTask(Task task) {
         return taskRepository.save(task);
     }
 
-    public void deleteTask(Task task) {
-        taskRepository.delete(task);
+    public void deleteTaskById(Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+
+    public void setAssigneeToTask(Task task, User userId) throws ResourceNotFoundException {
+
+        if (task != null) {
+            task.setAssignee(userId);
+        }
+
+        throw new ResourceNotFoundException("Task is null");
     }
 
     public Comment addComment(Long taskId, Comment comment) throws ResourceNotFoundException {
@@ -40,8 +58,12 @@ public class TaskService {
         return commentRepository.save(comment);
     }
 
-    public List<Comment> getComments(Long taskId) {
+    public List<Comment> getCommentsByTaskId(Long taskId) {
         return commentRepository.findByTaskId(taskId);
+    }
+
+    public List<Comment> getCommentsByTaskIdAndAuthorId(Long taskId, Long authorId){
+        return commentRepository.findByTaskIdAndAuthorId(taskId, authorId);
     }
 
     public Task updateTask(Long id, Task taskDetails) throws ResourceNotFoundException {
