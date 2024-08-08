e976dd2d95635a99ba68d8bd3b05d625f2449f61
amida <cain.wells@mail.ru>
2024-08-08 20:08:14 +0300

    Updated security filter chain for jwt auth

--- src/main/java/org/amida/task_managment_system/user_managment/config/SecurityConfig.java
+++ src/main/java/org/amida/task_managment_system/user_managment/config/SecurityConfig.java
@@ -3,26 +3,33 @@ package org.amida.task_managment_system.user_managment.config;
 import org.amida.task_managment_system.user_managment.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserRepository userRepository;
+    private final JwtRequestFilter jwtRequestFilter;
 
-    public SecurityConfig(UserRepository userRepository) {
+    public SecurityConfig(UserRepository userRepository, @Lazy JwtRequestFilter jwtRequestFilter) {
         this.userRepository = userRepository;
+        this.jwtRequestFilter = jwtRequestFilter;
     }
 
     @Bean
@@ -50,12 +57,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/register", "/api/users/login"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/register", "/api/users/login", "/api/tasks/create"))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+                        .requestMatchers("/api/tasks/create").authenticated()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
